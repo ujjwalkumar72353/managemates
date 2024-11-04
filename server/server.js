344a9214f931180f3a2450ec717510c8da9ffba6
ujjwalkumar7498 <ujjwalkumar4876@gmail.com>
2024-11-05 04:01:57 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -16,14 +16,6 @@ app.use("/api/tasks", tasksRoute);
 app.use("/api/notifications", notificationsRoute);
 
 // deployment config
-const path = require("path");
-__dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
 
 app.listen(port, () => console.log(`Node JS server listening on port ${port}`));
