be62f867d3188630d7c3abfd3a7b168c4cec8569
ujjwalkumar7498 <ujjwalkumar4876@gmail.com>
2024-11-05 04:29:44 +0530

    /changes

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,14 +1,8 @@
 const mongoose = require("mongoose");
-mongoose.connect(process.env.mongo_url);
 
-const connection = mongoose.connection;
 
-connection.on('connected', () => {
-    console.log('Mongo db connected successfully');
-})
-
-connection.on('error', (err) => {
-    console.log('Mongo db connection error: ', err);
-})
+mongoose.connect(process.env.mongo_url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected successfully'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 module.exports = mongoose;
